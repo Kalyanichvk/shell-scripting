236b775fa1875aed6a801f1951a8bda952f5f4e7
pawani <ch.v.kalyani14@gmail.com>
2024-05-18 06:28:33 +0530

    added extra files

--- nginx_status.sh
+++ nginx_status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-systemctl status nginx --no-pager >/dev/null
+systemctl status nginx --no-pager >/tmp/kalyani
 if [ $? -eq 0 ]; then
     echo "The Application Nginx Is Already Running...!!"
 else

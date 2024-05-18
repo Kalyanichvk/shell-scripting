9a88a47d6531e2aa7316bf8c68ca26bc2fcdd070
pawani <ch.v.kalyani14@gmail.com>
2024-05-18 06:45:36 +0530

    added extra files

--- restart_service_any.sh
+++ restart_service_any.sh
@@ -4,6 +4,7 @@ if [ $# -gt 0 ]; then
     for REGION in $*; do
         cowsay "[#-------${REGION}-------#]"
         aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r
+        echo "you have give $# parameters"
     done
 else
     cowsay "You Have Given $# paramters to this script, please provide arg Eg.us-east-1."

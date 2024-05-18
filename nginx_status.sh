6e766bdf6e47cb7ad0c44e3e69398c7336abf94b
pawani <ch.v.kalyani14@gmail.com>
2024-05-18 06:26:40 +0530

    added extra files

--- nginx_status.sh
+++ nginx_status.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-systemctl status nginx --no-pager
+systemctl status nginx --no-pager >/dev/null
 if [ $? -eq 0 ]; then
     echo "The Application Nginx Is Already Running...!!"
 else
     echo "Nginx is not running. Lets restart it...."
-    systemctl start nginx
-    systemctl status nginx --no-pager
+    systemctl start nginx >/dev/null
+    systemctl status nginx --no-pager >/dev/null
 fi
